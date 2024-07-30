8758a6926c98e5c58ab6b088ee4690a91757aaa8
Luis Mayta <luis.mayta@gmail.com>
2024-07-30 01:23:30 -0500

    Cambio de imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './assets/images/logo.svg';
+import logo from '/images/logo.svg';
 import {IconCart} from "./ui/index.js";
 import {useDispatch} from "react-redux";
 import {addProducts} from "./store/slices/CartSlice.jsx";
@@ -9,84 +9,84 @@ const data = [
         name: 'LUKATHER',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 299,
-        image: '/src/assets/images/guitarra_01.jpg',
+        image: '/images/guitarra_01.jpg',
     },
     {
         id: 2,
         name: 'SRV',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 190,
-        image: '/src/assets/images/guitarra_02.jpg',
+        image: '/images/guitarra_02.jpg',
     },
     {
         id: 3,
         name: 'BORLAND',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 309,
-        image: '/src/assets/images/guitarra_03.jpg',
+        image: '/images/guitarra_03.jpg',
     },
     {
         id: 4,
         name: 'VAI',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 80,
-        image: '/src/assets/images/guitarra_04.jpg',
+        image: '/images/guitarra_04.jpg',
     },
     {
         id: 5,
         name: 'THOMPSON\n',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 500,
-        image: '/src/assets/images/guitarra_05.jpg',
+        image: '/images/guitarra_05.jpg',
     },
     {
         id: 6,
         name: 'WHITE',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 680,
-        image: '/src/assets/images/guitarra_06.jpg',
+        image: '/images/guitarra_06.jpg',
     },
     {
         id: 7,
         name: 'COBAIN',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 65.3,
-        image: '/src/assets/images/guitarra_07.jpg',
+        image: '/images/guitarra_07.jpg',
     },
     {
         id: 8,
         name: 'DALE',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 900,
-        image: '/src/assets/images/guitarra_08.jpg',
+        image: '/images/guitarra_08.jpg',
     },
     {
         id: 9,
         name: 'KRIEGER',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 1200,
-        image: '/src/assets/images/guitarra_09.jpg',
+        image: '/images/guitarra_09.jpg',
     },
     {
         id: 10,
         name: 'CAMPBELL\n',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 680,
-        image: '/src/assets/images/guitarra_10.jpg',
+        image: '/images/guitarra_10.jpg',
     },
     {
         id: 11,
         name: 'REED',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 800,
-        image: '/src/assets/images/guitarra_11.jpg',
+        image: '/images/guitarra_11.jpg',
     },
     {
         id: 12,
         name: 'HAZEL',
         description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt',
         price: 100.5,
-        image: '/src/assets/images/guitarra_12.jpg',
+        image: '/images/guitarra_12.jpg',
     }
 ];
 
